3c502dee846d2506b1185c7cdf98da0a1ae86e50
Hao Zheng <hzheng19@illinois.edu>
2014-09-26 23:34:33 -0500

    Update SingleNumber.cpp

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -1,9 +1,6 @@
-class Solution {
-public:
-    int singleNumber(int A[], int n) {
-        int result = 0;
-        for(int i = 0; i<n; i++)
-            result = result^A[i];
-        return result;
-    }
-};
+int singleNumber(int A[], int n) {
+    int result = 0;
+    for(int i = 0; i<n; i++)
+        result = result^A[i];
+    return result;
+}
