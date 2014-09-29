815e3db11b2da2cde4d45c9c84f3825dc1aafe90
Hao Zheng <hzheng19@illinois.edu>
2014-09-29 18:27:55 -0500

    Update swapNodePairs.cpp

--- swapNodePairs.cpp
+++ swapNodePairs.cpp
@@ -7,24 +7,24 @@ ListNode *swapPairs(ListNode *head) {
         ListNode *p1 = head, *p2 = p1->next, *temp, *prev;
         while(p2 != NULL){
             
-            //change head
-            if(p1 == head)
-                head = p2;
+                //change head
+                if(p1 == head)
+                        head = p2;
                 
-            //swap node pair
-            temp = p2->next;
-            p2->next = p1;
-            p1->next = temp;
-            prev = p1;
+                //swap node pair
+                temp = p2->next;
+                p2->next = p1;
+                p1->next = temp;
+                prev = p1;
             
-            //iterate
-            p1 = temp;
-            if(p1 == NULL)
-                break;
-            else 
-                p2 = p1->next;
-            if(p2 != NULL)
-                prev->next = p2;
+                //iterate
+                p1 = temp;
+                if(p1 == NULL)
+                   break;
+                else 
+                   p2 = p1->next;
+                if(p2 != NULL)
+                   prev->next = p2;
         }
         return head;
-    }
+}
