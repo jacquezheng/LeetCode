a37797e3a7850728ca09455864f7342d7ba0df5d
Hao Zheng <hzheng19@illinois.edu>
2014-10-01 23:44:59 -0500

    Update removeElement.cpp

--- removeElement.cpp
+++ removeElement.cpp
@@ -1,3 +1,9 @@
+/* https://oj.leetcode.com/problems/remove-element/
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ */
+
+
 int removeElement(int A[], int n, int elem) {
     int count = 0;
     for(int i = n-1; i >= 0; i--){
